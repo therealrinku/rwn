5f20f81165991b61336981765f8345750bc8fc5e
therealrinku <rinkunited2032@gmail.com>
2026-05-29 12:50:08 +0545

    Simplify and make play pause work

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <script setup lang="ts">
 import { ref, computed } from "vue";
 import { invoke } from "@tauri-apps/api/core";
-
+import PlayIcon from "./components/icons/play-icon.vue";
+import PauseIcon from "./components/icons/pause-icon.vue";
 //  await invoke("greet", { name: name.value });
 
-const sec = 100;
+const sec = 1200;
 
-const timer = ref(sec);
+const time = ref(sec);
 const running = ref(false);
+const timer = ref(null);
 
-async function startTimer() {
+function toggleTimer() {
+  if (running.value) {
+    clearInterval(timer.value);
+    running.value = false;
+    return;
+  }
+
   running.value = true;
-  setInterval(() => {
-    if (timer.value > 0) {
-      timer.value--;
+  timer.value = setInterval(() => {
+    if (time.value > 0) {
+      time.value--;
+    } else {
+      clearInterval(timer.value);
     }
   }, 1000);
 }
 
 const formattedTime = computed(() => {
-  const mins = Math.floor(timer.value / 60);
-  const secs = timer.value % 60;
+  const mins = Math.floor(time.value / 60);
+  const secs = time.value % 60;
 
   const formattedMins = String(mins).padStart(2, "0");
   const formattedSecs = String(secs).padStart(2, "0");
@@ -28,7 +38,7 @@ const formattedTime = computed(() => {
 });
 
 const progress = computed(() => {
-  const elapsed = sec - timer.value;
+  const elapsed = sec - time.value;
   return Math.floor((elapsed / sec) * 100);
 });
 </script>
@@ -36,58 +46,13 @@ const progress = computed(() => {
 <template>
   <main class="h-screen w-screen text-sm tracking-wide">
     <div
-      v-if="running"
-      class="flex flex-col items-center justify-center gap-5 w-full h-full"
+      class="flex flex-col items-center justify-center gap-5 w-full h-full font-mono font-bold"
     >
-      <p class="text-4xl font-bold tracking-widest font-mono">
-        {{ formattedTime }}
-      </p>
-      <div class="fixed bottom-0 left-0 w-full h-2 bg-gray-200">
-        <div
-          :style="{ width: progress + '%' }"
-          class="w-10 h-full bg-green-400"
-        ></div>
-      </div>
-    </div>
+      <span class="text-4xl">{{ formattedTime }}</span>
 
-    <div
-      v-else
-      class="flex flex-col items-center justify-center gap-3 w-full h-full"
-    >
-      <button
-        class="cursor-pointer flex items-center gap-3 text-xl font-bold"
-        @click="startTimer"
-      >
-        <svg
-          height="38px"
-          width="38px"
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 512 512"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          enable-background="new 0 0 512 512"
-          fill="#000000"
-        >
-          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-          <g
-            id="SVGRepo_tracerCarrier"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          ></g>
-          <g id="SVGRepo_iconCarrier">
-            <g>
-              <g fill="#231F20">
-                <path
-                  d="m354.2,247.4l-135.1-92.4c-4.2-3.1-15.4-3.1-16.3,8.6v184.8c1,11.7 12.4,11.9 16.3,8.6l135.1-92.4c3.5-2.1 8.3-10.7 0-17.2zm-130.5,81.3v-145.4l106.1,72.7-106.1,72.7z"
-                ></path>
-                <path
-                  d="M256,11C120.9,11,11,120.9,11,256s109.9,245,245,245s245-109.9,245-245S391.1,11,256,11z M256,480.1 C132.4,480.1,31.9,379.6,31.9,256S132.4,31.9,256,31.9S480.1,132.4,480.1,256S379.6,480.1,256,480.1z"
-                ></path>
-              </g>
-            </g>
-          </g>
-        </svg>
-        <span class="text-4xl font-mono">{{ formattedTime }}</span>
+      <button class="cursor-pointer" @click="toggleTimer">
+        <play-icon v-if="!running" />
+        <pause-icon v-else />
       </button>
     </div>
   </main>
